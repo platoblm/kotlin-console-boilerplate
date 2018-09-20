34a45cc0dccbbc15f982838de56184428f453d8b
Plato Balomenakis <plato.blm@gmail.com>
2018-09-20 12:23:27 +0100

    v1

---
+++ README.md
@@ -0,0 +1,8 @@
+### kotlin-console-boilerplate
+
+Clone and start with App.kt and AppTest.kt
+
+Run with:
+```
+./gradlew run 
+```
---
+++ app/src/test/kotlin/example/AppTest.kt
@@ -0,0 +1,12 @@
+package example
+
+import org.junit.Assert
+import org.junit.Test
+
+class AppTest {
+
+    @Test
+    fun shouldPass() {
+        Assert.assertTrue(true)
+    }
+}
